d128263d959380f6cffcaec35e21f2b9139b4b8c
Elfi Yang <elfiyang16@gmail.com>
2020-09-21 22:42:35 +0100

    5. rest api endpoint

--- tweets/views.py
+++ tweets/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
 # Create your views here.
 
@@ -8,9 +8,18 @@ def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello World</h1>")
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    print(args, kwargs)
+    """
+    return json data consumed by JS 
+    """
+    data = {
+      "id": tweet_id,
+      # "image_path": obj.image_url
+    }
+    status = 200
     try:
       obj = Tweet.objects.get(id=tweet_id)
+      data["content"] = obj.content
     except: #try block to make it handle unfound id 
-      raise Http404
-    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
+      data["message"] = "Not Found"
+      status = 404    
+    return JsonResponse(data, status=status) # json.dumps content_type='application/json'
