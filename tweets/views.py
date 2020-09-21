8df399c7446da437133cc3f04c2008b5ed1eb269
Elfi Yang <elfiyang16@gmail.com>
2020-09-21 21:31:19 +0100

    3. url routing and dynamic url

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, Http404
+from .models import Tweet
 # Create your views here.
+
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    return HttpResponse("<h1>Hello World</h1>")
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    print(args, kwargs)
+    try:
+      obj = Tweet.objects.get(id=tweet_id)
+    except: #try block to make it handle unfound id 
+      raise Http404
+    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
