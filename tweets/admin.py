ba3d0666c3deca2dabf2926535ad4eeb79c5b074
Elfi Yang <elfiyang16@gmail.com>
2020-09-22 21:33:59 +0100

    25. add like as manytomany fields for user-tweet

--- tweets/admin.py
+++ tweets/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Tweet
+from .models import Tweet, TweetLike
+
+class TweetLikeAdmin(admin.TabularInline):
+    model = TweetLike
 
 class TweetAdmin(admin.ModelAdmin):
+  inlines = [TweetLikeAdmin]
   list_display = ["__str__", "user"] #a tuple of field names to display, as columns, on the change list page for the object:
   search_fields = ["content", "user__username", "user__email"]
   class Meta:
--- tweets/models.py
+++ tweets/models.py
@@ -4,11 +4,19 @@ import random
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL #build in user model
+
+
+class TweetLike(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+  timestamp = models.DateTimeField(auto_now_add=True)
 class Tweet(models.Model):
   #  hidden column: id = models.AutoField(primary_key=True)
   user = models.ForeignKey(User, on_delete=models.CASCADE) #many tweets to one user 
+  likes = models.ManyToManyField(User, related_name="tweet_user", blank=True, through=TweetLike)
   content = models.TextField(blank=True, null=True)
   image = models.FileField(upload_to="images/", blank=True, null=True) #can be blank, and null
+  timestamp = models.DateTimeField(auto_now_add=True)
   # def __str__(self):
     #     return self.content
   class Meta:
