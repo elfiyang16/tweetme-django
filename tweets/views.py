9cd5b755a8eca91164924c76b863b3ac6078dca7
Elfi Yang <elfiyang16@gmail.com>
2020-09-22 14:13:21 +0100

    16. add hanlde error function

--- tweets/views.py
+++ tweets/views.py
@@ -36,6 +36,9 @@ def tweet_create_view(request, *args, **kwargs):
     if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
       return redirect(next_url) 
     form = TweetForm() #init a new empty form 
+  if form.errors:
+    if request.is_ajax():
+      return JsonResponse(form.errors, status=400)
   return render(request, "components/form.html", context={"form": form})
   
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
