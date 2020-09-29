46b259fe861f0bba04731118ca5b4e6223abb3ba
Elfi Yang <elfiyang16@gmail.com>
2020-09-29 16:13:04 +0100

    35. render react act via dhango template

--- tweetme/urls.py
+++ tweetme/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path('api/tweets/<int:tweet_id>/delete', tweet_delete_view),
     path('api/tweets/action', tweet_action_view),
     path('api/tweets/', include('tweets.urls')),
-    path('react/', TemplateView.as_view(template_name='react.html')),
+    path('react/', TemplateView.as_view(template_name='react_via_dj.html')),
 
 
 
